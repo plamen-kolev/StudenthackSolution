63eb2618e19274739981f58e2f38c981d04ef82d
Daniel Baranowski <d.m.baranowski@ncl.ac.uk>
2017-03-11 12:13:16 +0000

    omfg

---
+++ video-quickstart-node/index.js
@@ -0,0 +1,107 @@
+/*
+ Load Twilio configuration from .env config file - the following environment
+ variables should be set:
+ process.env.TWILIO_ACCOUNT_SID
+ process.env.TWILIO_API_KEY
+ process.env.TWILIO_API_SECRET
+ process.env.TWILIO_CONFIGURATION_SID
+ */
+require('dotenv').load();
+const uuidV4 = require('uuid/v4');
+
+var http = require('http');
+var path = require('path');
+var AccessToken = require('twilio').AccessToken;
+var VideoGrant = AccessToken.VideoGrant;
+var express = require('express');
+var randomUsername = require('./randos');
+
+var Parse = require('parse/node');
+
+Parse.initialize("Dott aMate","abc123","Shaman10201");
+Parse.serverURL = 'http://95.85.22.29:80/parse';
+
+
+
+var VoiceRecording = Parse.Object.extend("VoiceRecording");
+
+
+// Create Express webapp
+var bodyParser = require("body-parser");
+var app = express();
+app.use('/public', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({extended: false, limit: '500mb'}));
+app.use(bodyParser.json({limit: '500mb'}));
+
+/*
+ Generate an Access Token for a chat application user - it generates a random
+ username for the client requesting a token, and takes a device ID as a query
+ parameter.
+ */
+app.get('/token', function (request, response) {
+    var identity = randomUsername();
+
+    // Create an access token which we will sign and return to the client,
+    // containing the grant we just created
+    var token = new AccessToken(
+        process.env.TWILIO_ACCOUNT_SID,
+        process.env.TWILIO_API_KEY,
+        process.env.TWILIO_API_SECRET
+    );
+
+    // Assign the generated identity to the token
+    token.identity = identity;
+
+    //grant the access token Twilio Video capabilities
+    var grant = new VideoGrant();
+    grant.configurationProfileSid = process.env.TWILIO_CONFIGURATION_SID;
+    token.addGrant(grant);
+
+    // Serialize the token to a JWT string and include it in a JSON response
+    response.send({
+        identity: identity,
+        token: token.toJwt()
+    });
+});
+
+var fs = require('fs');
+app.post('/save_recording', function (req, res) {
+    var buf = new Buffer(req.body.blob, 'base64'); // decode
+    var fileName = uuidV4() + '.wav';
+    /*fs.writeFile(__dirname + "/public/" + fileName, buf, function (err) {
+        if (err) {
+            console.log("err", err);
+        } else {
+            return res.json({'status': 'success'});
+        }
+    })*/
+
+
+    var voiceRecording = new VoiceRecording();
+    voiceRecording.set("sessionId", 1);
+    voiceRecording.set("data", new Parse.File( fileName,{ base64: req.body.blob } , "wav" ) );
+
+    voiceRecording.save(null, {
+        success: function (data) {
+            // Execute any logic that should take place after the object is saved.
+            console.log('New object created with objectId: ' + data.id);
+            return res.json({'status': 'success'});
+        },
+        error: function (gameScore, error) {
+            // Execute any logic that should take place if the save fails.
+            // error is a Parse.Error with an error code and message.
+            console.log('Failed to create new object, with error code: ' + error.message);
+        }
+    });
+
+});
+
+// Create http server and run it
+var server = http.createServer(app);
+var port = process.env.PORT || 3000;
+server.listen(port, function () {
+    console.log('Express server running on *:' + port);
+});
---
+++ video-quickstart-node/public/quickstart.js
@@ -0,0 +1,200 @@
+var videoClient;
+var activeRoom;
+var previewMedia;
+var identity;
+var roomName;
+
+// Check for WebRTC
+if (!navigator.webkitGetUserMedia && !navigator.mozGetUserMedia) {
+    alert('WebRTC is not available in your browser.');
+}
+
+// When we are about to transition away from this page, disconnect
+// from the room, if joined.
+window.addEventListener('beforeunload', leaveRoomIfJoined);
+
+$.getJSON('/token', function (data) {
+    identity = data.identity;
+
+    // Create a Video Client and connect to Twilio
+    videoClient = new Twilio.Video.Client(data.token);
+    document.getElementById('room-controls').style.display = 'block';
+
+    // Bind button to join room
+    document.getElementById('button-join').onclick = function () {
+        roomName = document.getElementById('room-name').value;
+        if (roomName) {
+            log("Joining room '" + roomName + "'...");
+
+            videoClient.connect({to: roomName}).then(roomJoined,
+                function (error) {
+                    log('Could not connect to Twilio: ' + error.message);
+                });
+        } else {
+            alert('Please enter a room name.');
+        }
+    };
+
+    // Bind button to leave room
+    document.getElementById('button-leave').onclick = function () {
+        log('Leaving room...');
+        activeRoom.disconnect();
+    };
+});
+
+var mediaRecorder;
+var ourFirstParticipant = true;
+var blob;
+
+function startRecordingClientAudio() {
+    navigator.getUserMedia =
+        ( navigator.getUserMedia ||
+        navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia ||
+        navigator.msGetUserMedia);
+
+    var audioCtx = new (window.AudioContext || webkitAudioContext)();
+    if (navigator.getUserMedia) {
+        console.log('getUserMedia supported.');
+
+        var constraints = {audio: true};
+        var chunks = [];
+
+        var onSuccess = function(stream) {
+            mediaRecorder = new MediaRecorder(stream);
+            mediaRecorder.start();
+            console.log(mediaRecorder.state);
+            console.log("recorder started");
+
+            mediaRecorder.ondataavailable = function (e) {
+                chunks.push(e.data);
+            };
+
+            mediaRecorder.onstop = function (e) {
+                console.log("data available after MediaRecorder.stop() called.");
+                blob = new Blob(chunks, {'type': 'audio/ogg; codecs=opus'});
+                chunks = [];
+                blobToBase64(blob);
+            };
+        };
+
+        var onError = function(err) {
+            console.log('The following error occured: ' + err);
+        };
+
+        navigator.getUserMedia(constraints, onSuccess, onError);
+    } else {
+        console.log('getUserMedia not supported on your browser!');
+    }
+}
+
+//Send blob to api
+function sendToApi(base64) { // encode
+    var update = {'blob': base64};
+
+    $.ajax({
+        type: "POST",
+        url: '/save_recording',
+        data: update,
+        success: function (new_recording) {
+            console.log("success");
+        },
+        dataType: 'application/json'
+    });
+}
+
+// converts blob to base64
+var blobToBase64 = function (blob) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        var dataUrl = reader.result;
+        var base64 = dataUrl.split(',')[1];
+        sendToApi(base64);
+    };
+    reader.readAsDataURL(blob);
+};
+
+// Successfully connected!
+function roomJoined(room) {
+    activeRoom = room;
+
+    log("Joined as '" + identity + "'");
+    document.getElementById('button-join').style.display = 'none';
+    document.getElementById('button-leave').style.display = 'inline';
+
+    // Draw local video, if not already previewing
+    if (!previewMedia) {
+        room.localParticipant.media.attach('#local-media');
+    }
+
+    function onData(data) {
+        if (data.end) streamer.end();
+        else streamer.append(data);
+    }
+
+
+    room.participants.forEach(function (participant) {
+        log("Already in Room: '" + participant.identity + "'");
+        participant.media.attach('#remote-media');
+    });
+
+    // When a participant joins, draw their video on screen
+    room.on('participantConnected', function (participant) {
+        log("Joining: '" + participant.identity + "'");
+        participant.media.attach('#remote-media');
+        if (ourFirstParticipant) {
+            startRecordingClientAudio();
+            ourFirstParticipant = false;
+        }
+
+    });
+
+    // When a participant disconnects, note in log
+    room.on('participantDisconnected', function (participant) {
+        log("Participant '" + participant.identity + "' left the room");
+        participant.media.detach();
+        mediaRecorder.stop();
+    });
+
+    // When we are disconnected, stop capturing local video
+    // Also remove media for all remote participants
+    room.on('disconnected', function () {
+        log('Left');
+        room.localParticipant.media.detach();
+        room.participants.forEach(function (participant) {
+            participant.media.detach();
+        });
+        activeRoom = null;
+        document.getElementById('button-join').style.display = 'inline';
+        document.getElementById('button-leave').style.display = 'none';
+    });
+}
+
+//  Local video preview
+document.getElementById('button-preview').onclick = function () {
+    if (!previewMedia) {
+        previewMedia = new Twilio.Video.LocalMedia();
+        Twilio.Video.getUserMedia().then(
+            function (mediaStream) {
+                previewMedia.addStream(mediaStream);
+                previewMedia.attach('#local-media');
+            },
+            function (error) {
+                console.error('Unable to access local media', error);
+                log('Unable to access Camera and Microphone');
+            });
+    }
+};
+
+// Activity log
+function log(message) {
+    var logDiv = document.getElementById('log');
+    logDiv.innerHTML += '<p>&gt;&nbsp;' + message + '</p>';
+    logDiv.scrollTop = logDiv.scrollHeight;
+}
+
+function leaveRoomIfJoined() {
+    if (activeRoom) {
+        activeRoom.disconnect();
+    }
+}
